c90c48f4324ebac4480285d24cef0f9ac90cd036
Vince Mendella <vmendella@gmail.com>
2021-06-15 20:35:38 -0500

    add **/*.scss & *.scss patterns to compile all files with .scss

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,8 @@ const gulp = require('gulp');
 const sass = require('gulp-sass');
 
 gulp.task("sass", function(done) {
-  return gulp.src('./src/sass/main.scss')
+  return gulp.
+  src(['./src/sass/**/*.scss', '!./src/sass/widget.scss'])
     .pipe(sass())
     .pipe(gulp.dest('./dist/css'));
   done();
